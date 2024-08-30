11d2eb1982a12c1a8c53df4844318920dc30f568
somsakphu <somsak.9.p@gmail.com>
2024-08-31 01:41:46 +0700

    1.41
    1.41

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const debug = require('debug');
 const morgan = require('morgan')
 const app = express();
 const port = 2500; 
-
+const path = require('path');
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname,"/public/")));
 app.get("/",(req,res)=>{
 
     res.send('hello 112 ');
